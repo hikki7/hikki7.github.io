e5c86376245a673e4811fbaea0af9700582deb8e
amaimon <hyjk.0810@gmail.com>
2018-08-11 15:40:36 +0900

    webGL

--- HP/portfolio 3/js/main.js
+++ HP/portfolio 3/js/main.js
@@ -1,75 +1,10 @@
-
-// A wind direction vector
-var wind;
-// Circle position
-var position;
-
 function setup() {
-  var canvas=createCanvas(windowWidth, windowHeight);
+  var canvas=createCanvas(windowWidth, windowHeight,WEBGL);
   canvas.parent('canvas-header');
-  // Request the data from apixu.com
-  var url = 'https://api.github.com';
-
-  loadJSON(url, gotWeather);
-  // Circle starts in the middle
-  position = createVector(width/2, height/2);
-  // wind starts as (0,0)
-  wind = createVector();
-}
-
-function draw() {
-  background(0);
-
-  // This section draws an arrow pointing in the direction of wind
-  push();
-  translate(32, height - 32);
-  // Rotate by the wind's angle
-  //heading()で回転するってこと
-  rotate(wind.heading() + PI/2);
-  noStroke();
-  fill(40);
-  ellipse(0, 0, 48, 48);
-
-  stroke(45, 123, 182);
-  strokeWeight(3);
-  line(0, -16, 0, 16);
-
-  noStroke();
-  fill(45, 123, 182);
-  triangle(0, -18, -6, -10, 6, -10);
-  pop();
-
-  // Move in the wind's direction
-  position.add(wind);
-
-  stroke(0);
-  fill(51);
-  ellipse(position.x, position.y, 16, 16);
-
-  if (position.x > width)  position.x = 0;
-  if (position.x < 0)      position.x = width;
-  if (position.y > height) position.y = 0;
-  if (position.y < 0)      position.y = height;
 
 
 }
 
-function gotWeather(weather) {
+function draw(){
 
-  console.log('きた？？');
-  // // Get the angle (convert to radians)
-  // var angle = radians(Number(weather.current.wind_degree));
-  // // Get the wind speed
-  // var windmag = Number(weather.current.wind_mph);
-  //
-  // // Display as HTML elements
-  // var temperatureDiv = createDiv(floor(weather.current.temp_f) + '&deg;');
-  // var windDiv = createDiv("WIND " + windmag + " <small>MPH</small>");
-  //
-  // // Make a vector
-  // wind = p5.Vector.fromAngle(angle);
-}
-
-function windowResized(){
-    resizeCanvas(windowWidth,windowHeight);
 }
